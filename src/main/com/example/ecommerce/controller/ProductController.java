ab584eaccf15e29adbb58581e61c63d64b3acfb7
vikas <gowdavikas0105@gmail.com>
2023-12-21 12:53:44 +0530

    adding all source codes

---
+++ src/main/com/example/ecommerce/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public String listProducts(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "product/list";
+    }
+}
+
---
+++ src/main/com/example/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Custom queries or methods if needed
+}
+
