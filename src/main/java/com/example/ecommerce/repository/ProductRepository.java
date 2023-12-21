17cd1f71b3993c6600e2c1de68e62ef196ea5706
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2023-12-21 16:02:42 +0530

    Update ProductRepository.java

--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -6,5 +6,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // You can add custom query methods if needed
 }
-
-
