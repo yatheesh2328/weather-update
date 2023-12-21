7b0d791676df71c5786ffe39f183111b357a9a70
vikas <gowdavikas0105@gmail.com>
2023-12-21 21:00:56 +0530

    removed all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecommerce
-test e commerce website
--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // You can add custom query methods if needed
-}
