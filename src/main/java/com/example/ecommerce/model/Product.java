552d71b310c46386b5db7ffd0977a42980b017f9
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2023-12-21 14:51:32 +0530

    Update Product.java

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -15,38 +15,7 @@ public class Product {
     private String name;
     private String description;
     private double price;
+    private int quantity; // New field
 
     // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
