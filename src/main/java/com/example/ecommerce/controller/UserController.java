8ed0a04f371b9ebc5ce8170a337e5b7f63178049
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2023-12-21 14:56:19 +0530

    Create UserController.java

---
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    // Existing code...
+
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        // Add logic to save the user to the database
+        return "redirect:/login";
+    }
+}
