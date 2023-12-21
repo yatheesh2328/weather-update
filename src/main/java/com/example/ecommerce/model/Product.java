5f93a50e9459c05f096a28d7547c5640ee1e6140
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2023-12-21 15:24:16 +0530

    Update Product.java

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -15,7 +15,7 @@ public class Product {
     private String name;
     private String description;
     private double price;
-    private int quantity; // New field
+    private int quantity;
 
     // Getters and setters
 }
