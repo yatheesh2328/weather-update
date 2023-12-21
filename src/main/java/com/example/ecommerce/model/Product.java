a954bdd5323f8484f5f56e27673a3339d349eccd
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2023-12-21 14:37:14 +0530

    Update Product.java

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,6 +16,37 @@ public class Product {
     private String description;
     private double price;
 
-    // getters and setters
-}
+    // Getters and setters
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
